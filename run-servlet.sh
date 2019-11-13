4948f877b5d73194475c46965a18240bd00316f4
Henrik Sandklef <hesa@sandklef.com>
2019-11-13 11:17:43 +0100

    log enabled and used

--- run-servlet.sh
+++ run-servlet.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+LOG_DIR="/tmp/lifegame-web"
+ERR_LOG="${LOG_DIR}/lifegame-err.log"
+INFO_LOG="${LOG_DIR}/lifegame.log"
+
+mkdir -p "${LOG_DIR}"
+
 while(true)
 do
-    java -jar lib/winstone.jar --webroot=www --ajp13Port=8010 --httpPort=8081
+    echo "lifegame:$0 $(date) | starting server" > "${INFO_LOG}"
+    java -jar lib/winstone.jar --webroot=www --ajp13Port=8010 --httpPort=8081 > "${INFO_LOG}" 2> "${ERR_LOG}"
 done
